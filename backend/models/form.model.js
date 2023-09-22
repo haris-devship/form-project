77348a0676d3060b43bbecbc4916305478117122
haris-devship <haris@devship.in>
2023-09-22 19:57:18 +0530

    created model for form

--- backend/models/form.model.js
+++ backend/models/form.model.js
@@ -1,29 +1,29 @@
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
-// const formSchema = new mongoose.Schema({
-//   name: {
-//     type: String,
-//     required: true,
-//   },
-//   age: {
-//     type: Number,
-//     required: true,
-//   },
-//   url: {
-//     type: String,
-//     required: true,
-//   },
-//   file: {
-//     type: "string",
-//     required: true,
-//   },
-//   status: {
-//     type: String,
-//     enum: ["active", "not active"],
-//     required: true,
-//   },
-// });
+const formSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: Number,
+    required: true,
+  },
+  url: {
+    type: String,
+    required: true,
+  },
+  file: {
+    type: "string",
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ["active", "not active"],
+    required: true,
+  },
+});
 
-// const FormModel = mongoose.model("Form", formSchema);
+const FormModel = mongoose.model("Form", formSchema);
 
-// module.exports = { FormModel };
+module.exports = { FormModel };
