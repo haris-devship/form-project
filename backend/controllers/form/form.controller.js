5674ac8eb6b35869988feefa26fe59cda1054a85
haris-devship <haris@devship.in>
2023-09-22 19:57:57 +0530

    created controller for form

--- backend/controllers/form/form.controller.js
+++ backend/controllers/form/form.controller.js
@@ -1,21 +1,21 @@
-// const { FormModel } = require("../../models/form.model");
+const { FormModel } = require("../../models/form.model");
 
-// module.exports = {
-//   async addForm(req, res) {
-//     try {
-//         const formDataArray = req.body;
-//       for (const formData of formDataArray) {
-//         const newForm = new FormModel(formData);
-//         await newForm.save();
-//       }
-//       res.status(200).send({
-//         message: "Form Added Successfully",
-//       });
-//     } catch (err) {
-//       console.log(err);
-//       res.status(500).send({
-//         message: "Error Adding Form",
-//       });
-//     }
-//   },
-// };
+module.exports = {
+  async addForm(req, res) {
+    try {
+        const formDataArray = req.body;
+      for (const formData of formDataArray) {
+        const newForm = new FormModel(formData);
+        await newForm.save();
+      }
+      res.status(200).send({
+        message: "Form Added Successfully",
+      });
+    } catch (err) {
+      console.log(err);
+      res.status(500).send({
+        message: "Error Adding Form",
+      });
+    }
+  },
+};
