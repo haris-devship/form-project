dabd371e2e0920a6a21c95e636ff5fe3db557387
haris-devship <haris@devship.in>
2023-09-22 19:51:31 +0530

    created the functionality

--- frontend/src/Components/DynamicForm.jsx
+++ frontend/src/Components/DynamicForm.jsx
@@ -96,7 +96,103 @@ const DynamicForm = () => {
 
   return (
     <>
-     
+      <Stack gap={2} w={"100%"}>
+        {forms?.map((form, index) => {
+          return (
+            <Box
+              m={"auto"}
+              key={index}
+              display={"flex"}
+              gap={3}
+              p={4}
+              border={"1px solid #ccc"}
+              w={"100%"}
+              borderRadius={"md"}
+              alignItems={"center"}
+              justifyContent={"space-evenly"}
+            >
+              <FormControl>
+                <FormLabel>Name</FormLabel>
+                <Input
+                  value={form.name}
+                  onChange={(e) => handleChange(index, "name", e.target.value)}
+                />
+              </FormControl>
+
+              <FormControl>
+                <FormLabel>Age</FormLabel>
+                <Input
+                  type="number"
+                  value={form.age}
+                  onChange={(e) => handleChange(index, "age", e.target.value)}
+                />
+              </FormControl>
+
+              <FormControl>
+                <FormLabel>Enter URL</FormLabel>
+                <Input
+                  type="url"
+                  value={form.url}
+                  onChange={(e) => handleChange(index, "url", e.target.value)}
+                />
+                {/* {!urlPattern.test(form.url) && (
+                  <FormHelperText color="red">Invalid URL</FormHelperText>
+                )} */}
+              </FormControl>
+
+              <FormControl>
+                <FormLabel>File Upload</FormLabel>
+                <input
+                  type="file"
+                  onChange={(e) =>
+                    handleChange(index, "file", e.target.files[0])
+                  }
+                />
+              </FormControl>
+
+              <FormControl>
+                <FormLabel>Status</FormLabel>
+                <Select
+                  value={form.status}
+                  onChange={(e) =>
+                    handleChange(index, "status", e.target.value)
+                  }
+                >
+                  <option value="active">Active</option>
+                  <option value="not active">Not Active</option>
+                </Select>
+              </FormControl>
+
+              <IconButton
+                isDisabled={forms.length == 1}
+                top={3}
+                icon={<DeleteIcon />}
+                colorScheme="red"
+                onClick={() => handleRemoveForm(index)}
+              />
+            </Box>
+          );
+        })}
+
+        <Box
+          display={"flex"}
+          alignItems={"center"}
+          justifyContent={"left"}
+          mt={2}
+          gap={2}
+        >
+          <Button
+            alignSelf={"left"}
+            leftIcon={<AddIcon />}
+            onClick={handleAddForm}
+          >
+            Add Form
+          </Button>
+          <Button onClick={handleSubmit} colorScheme="blue">
+            SUBMIT
+          </Button>
+        </Box>
+      </Stack>
     </>
   );
 };
